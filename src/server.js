2bf6b43d4a9dc3e2bd17c4801058f2a87d36d345
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-28 04:49:00 -0500

    feat: complete front-end

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  const homePage = path.join(__dirname, "..", "public", "index.html");
+  res.sendFile(homePage);
+});
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.set("port", process.env.PORT || 4100);
 
 const server = app.listen(app.get("port"), () => {

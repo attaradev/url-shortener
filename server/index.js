2604dc312d8a357995c2cf8741331912cb18d74f
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-28 06:03:47 -0500

    feat: connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const nanoid = require("nanoid");
+const { MongoClient } = require("mongodb");
+
+const databaseURL = process.env.DATABASE;
 
 const app = express();
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+MongoClient.connect(
+  databaseURL,
+  { useNewUrlParser: true }
+)
+  .then(client => {
+    app.locals.db = client.db("shortener");
+  })
+  .catch(() => console.error("Failed to connect to database"));
 
 app.get("/", (req, res) => {
   const homePage = path.join(__dirname, "..", "public", "index.html");
@@ -23,8 +39,28 @@ app.post("/new", (req, res) => {
     if (err) {
       return res.status(404).send({ error: "Address not found" });
     }
+
+    const { db } = req.app.locals;
+    shortenURL(db, originalUrl.href)
+      .then(result => {
+        const doc = result.value;
+        res.json({
+          original_url: doc.original_url,
+          short_id: doc.short_id
+        });
+      })
+      .catch(console.error);
   });
 });
+
+const shortenURL = (db, url) => {
+  const shortenedURLs = db.collection("shortenedURLs");
+  return shortenedURLs.findOneAndUpdate(
+    { originalUrl: url },
+    { $setOnInsert: { originalUrl: url, short_id: nanoid(7) } },
+    { returnOriginal: false, upsert: true }
+  );
+};
 
 app.set("port", process.env.PORT || 4100);
 
